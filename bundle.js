2a9e0ba4f854fa335ce729413ce02706a4c582a9
Dylan <dylan.patrick.cheng@gmail.com>
2017-04-08 17:57:30 -0700

    adjust paddle so whole paddle is always on screen

--- bundle.js
+++ bundle.js
@@ -254,23 +254,29 @@ class Paddle {
   constructor(ctx) {
     this.ctx = ctx;
     this.leftCoord = 0;
-    this.borders = [this.leftCoord, this.leftCoord + 150, 700, 720];
+    this.width = 150;
+    this.borders = [this.leftCoord, this.leftCoord + this.width, 700, 720];
     this.canvas = document.getElementById("game-canvas");
     this.canvas.addEventListener("mousemove", this.handleHover.bind(this));
   }
 
   draw() {
     this.ctx.fillStyle="blue";
-    this.ctx.fillRect(this.leftCoord, 700, 150, 20);
+    this.ctx.fillRect(this.leftCoord, 700, this.width, 20);
   }
 
   handleHover(e) {
-    this.leftCoord = e.screenX - 75;
+    this.leftCoord = e.screenX - (this.width / 2);
+    if (this.leftCoord < 0) {
+      this.leftCoord = 0;
+    } else if (this.leftCoord > (1000 - this.width)) {
+      this.leftCoord = (1000 - this.width);
+    }
     this.updateBorders();
   }
 
   updateBorders() {
-    this.borders = [this.leftCoord, this.leftCoord + 150, 700, 720];
+    this.borders = [this.leftCoord, this.leftCoord + this.width, 700, 720];
   }
 
 }

2e6e7becac69067ad49a214f2fb89a6c55aeeffa
Dylan <dylan.patrick.cheng@gmail.com>
2017-04-26 22:57:55 -0700

    add beginning of turning paddle into a baseball bat

--- bundle.js
+++ bundle.js
@@ -348,13 +348,19 @@ class Paddle {
     this.borders = [this.leftCoord, this.leftCoord + this.width, this.height, this.height + 20];
     this.canvas = document.getElementById("game-canvas");
     this.canvas.addEventListener("mousemove", this.handleHover.bind(this));
-  }
+    // this.img = new Image;
+    // this.img.setAttribute("src", "http://vignette2.wikia.nocookie.net/fallout/images/6/66/Cedar_Baseball_Bat.png/revision/latest?cb=20150619160520");
+    // this.img.setAttribute("style", `width: ${this.width}`);
+    // this.img.setAttribute("height", 20);
 
+  }
   draw() {
     this.ctx.fillStyle="black";
     this.ctx.fillRect(this.leftCoord, this.height, this.width, 20);
     this.ctx.fillStyle="blue";
     this.ctx.fillRect(this.leftCoord + this.borderWidth, this.height + this.borderWidth, this.width - (this.borderWidth * 3), 20 - (this.borderWidth * 2));
+
+    // this.ctx.drawImage(this.img, 0, 0);
   }
 
   handleHover(e) {

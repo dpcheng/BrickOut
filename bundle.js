e5e600a49d016a9e67d7aeeb7aaf588e625358cf
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-20 23:52:42 -0700

    remove comments from Ball class

--- bundle.js
+++ bundle.js
@@ -313,30 +313,22 @@ class Ball {
   }
 
   checkBrickContact(brickBorders) {
-
-    // This will check whether the right or left border of the ball is between the brick's right and left border
     if (
       (this.borders[2] > brickBorders[2] && this.borders[2] < brickBorders[3]) ||
       (this.borders[3] > brickBorders[2] && this.borders[3] < brickBorders[3]) ) {
 
-      // Check whether the top (or bottom) border of the ball is between the brick's top and bottom border
       if (
         (this.borders[0] > brickBorders[0] && this.borders[0] < brickBorders[1]) ||
         (this.borders[1] > brickBorders[0] && this.borders[1] < brickBorders[1])
         ) {
 
-        // Once I determine part of the ball is within the brick's borders, we check which of the brick's borders the ball is bouncing off of
-
-        // This checks if the right (or left) side of the ball is in contact with the left (or right) side of the brick.
         if (
           ( (this.borders[1] > brickBorders[0] - 2) && (this.borders[1] < brickBorders[0] + 2) ) ||
           ( (this.borders[0] > brickBorders[1] - 2) && (this.borders[0] < brickBorders[1] + 2) )
           ) {
 
-          // reverse ball's horizontal velocity
           this.velocity[0] = -this.velocity[0];
 
-          // Check if the ball is closer to right or left border. Then I adjust the position of the ball so the ball is no longer partially within the brick
           if (Math.abs(brickBorders[0] - this.posX) >
           Math.abs(brickBorders[1] - this.posX)) {
             this.posX = brickBorders[1] + this.radius;
@@ -344,20 +336,18 @@ class Ball {
             this.posX = brickBorders[0] - this.radius;
           }
 
-        // This block will run if the top (or bottom) side of the ball is in contact with the bottom (or top) of the brick
         } else {
-          this.velocity[1] = -this.velocity[1]; // reverse ball's vertical velocity
+          this.velocity[1] = -this.velocity[1];
 
-          // Check if the ball is closer to top or bottom border. Then I adjust the position of the ball so a portion of the ball is not within the brick
           if (Math.abs(brickBorders[2] - this.posY) >
           Math.abs(brickBorders[3] - this.posY)) {
             this.posY = brickBorders[3] + this.radius;
           } else {
             this.posY = brickBorders[2] - this.radius;
           }
+
         }
 
-      // The position of the ball is now different, so I update the ball's borders
       this.updateBorders();
       return true;
       }
